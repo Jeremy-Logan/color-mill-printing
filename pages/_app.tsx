83f1e6909c75209fe6e1754a93f3773a8e1beea6
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2022-07-17 12:16:50 -0700

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AuthContextProvider } from '../context/authContext';
+import Navbar from '../components/ui/navbar';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <AuthContextProvider><Navbar><Component {...pageProps} /></Navbar></AuthContextProvider>
 }
 
 export default MyApp
